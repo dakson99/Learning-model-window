998c69c6360cd7a5a796bdf01f024cd98f4ec075
Darko <72525936+dakson99@users.noreply.github.com>
2024-06-10 19:51:04 +0200

    The first commit, clicking on the button opens a popup window, where I used access via the add and remove classes

--- script.js
+++ script.js
@@ -5,6 +5,23 @@ const overlay = document.querySelector('.overlay');
 const btnCloaseModel = document.querySelector('.close-modal');
 const btnOpenModel = document.querySelectorAll('.show-modal');
 console.log(btnOpenModel);
+
+const openModel = function () {
+  console.log('Button clicked');
+  model.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModel = function () {
+  model.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+//Open model
 for (let i = 0; i < btnOpenModel.length; i++) {
-  console.log(btnOpenModel[i].textContent);
+  btnOpenModel[i].addEventListener('click', openModel);
 }
+
+//Close model
+btnCloaseModel.addEventListener('click', closeModel);
+overlay.addEventListener('click', closeModel);
